dd3d0461225d5ba0f859f690c0e4bf4892186e1a
hasandel01 <hasan_deliktas01@outlook.com>
2025-05-12 05:46:44 +0300

    Major UI changes and vercel deploy try.

--- meetmeout-web/src/features/event/EventDetails/Chat/Chat.tsx
+++ meetmeout-web/src/features/event/EventDetails/Chat/Chat.tsx
@@ -3,7 +3,7 @@ import { Message } from "../../../../types/Message";
 import styles from "./Chat.module.css"
 import { useUserContext } from "../../../../context/UserContext";
 import SockJS from 'sockjs-client';
-import { Client, IMessage} from "@stomp/stompjs";
+import { Client} from "@stomp/stompjs";
 import {toast} from 'react-toastify';
 import axiosInstance from "../../../../axios/axios";
 
@@ -27,44 +27,45 @@ const Chat: React.FC<ChatProps> = ({eventId}) => {
       },[eventId])
 
 
-      useEffect(() => {
-                const token = localStorage.getItem("accessToken");
-                const baseUrl = import.meta.env.VITE_SOCKET_BASE_URL;
+      
+       useEffect(() => {
+            const token = localStorage.getItem("accessToken");
+            const baseUrl = import.meta.env.VITE_SOCKET_BASE_URL;
 
-                const socket = new SockJS(`${baseUrl}/ws`);
+            const socket = new SockJS(`${baseUrl}/ws`);
 
-                const client = new Client({
-                webSocketFactory: () => socket,
-                connectHeaders: {
-                    Authorization: `Bearer ${token}`,
-                },
-                debug: (str) => console.log("[WebSocket DEBUG]", str),
-                reconnectDelay: 5000,
-                onConnect: () => {
-                    toast.success("WebSocket connected ✅");
+            const client = new Client({
+            webSocketFactory: () => socket,
+            connectHeaders: {
+                Authorization: `Bearer ${token}`,
+            },
+            debug: (str) => console.log("[WebSocket DEBUG]", str),
+            reconnectDelay: 5000,
+            onConnect: () => {
+                toast.success("WebSocket connected ✅");
 
-                    client.subscribe(`/topic/chat/event/${eventId}`, (message: IMessage) => {
-                    const payload = JSON.parse(message.body);
-                    setMessages((prev) => [...prev, payload.message]);
-                    });
-                },
-                onStompError: (frame) => {
-                    console.error("STOMP error:", frame);
-                    toast.error("WebSocket STOMP error ❌");
-                },
-                onWebSocketError: (event) => {
-                    console.error("WebSocket error:", event);
-                    toast.error("WebSocket connection error ⚠️");
-                },
+                client.subscribe(`/topic/chat/event/${eventId}`, (message: Message) => {
+                const payload = JSON.parse(message.body);
+                setMessages((prev) => [...prev, payload.message]);
                 });
+            },
+            onStompError: (frame) => {
+                console.error("STOMP error:", frame);
+                toast.error("WebSocket STOMP error ❌");
+            },
+            onWebSocketError: (event) => {
+                console.error("WebSocket error:", event);
+                toast.error("WebSocket connection error ⚠️");
+            },
+            });
 
-                clientRef.current = client;
-                client.activate();
+            clientRef.current = client;
+            client.activate();
 
-                return () => {
-                client.deactivate();
-                };
-            }, [eventId]);
+            return () => {
+            client.deactivate();
+            };
+        }, [eventId]);
 
       
 
--- meetmeout-web/src/features/event/EventDetails/EventDetails.tsx
+++ meetmeout-web/src/features/event/EventDetails/EventDetails.tsx
@@ -110,7 +110,7 @@ const EventDetails = () => {
     try {
 
     
-
+    /*
       const response = await 
         axios
         .get(`https://api.openweathermap.org/data/3.0/onecall?lat=${event.latitude}&lon=${event.longitude}&exclude=minutely,hourly&appid=${apiKey}&units=metric`, {
@@ -118,7 +118,7 @@ const EventDetails = () => {
 
         console.log(response.data);
         setWeather(response.data);
-    
+    */
 
     }catch(error) {
       toast.error("Error getting weather info");

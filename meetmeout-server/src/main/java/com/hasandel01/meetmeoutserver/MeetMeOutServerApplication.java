654c2949f4fd726371564359b0a181aacaa7fe9e
hasandel01 <hasan_deliktas01@outlook.com>
2025-06-10 18:35:43 +0300

    Render deploy try. timezone fix.

--- meetmeout-server/src/main/java/com/hasandel01/meetmeoutserver/MeetMeOutServerApplication.java
+++ meetmeout-server/src/main/java/com/hasandel01/meetmeoutserver/MeetMeOutServerApplication.java
@@ -1,7 +1,5 @@
 package com.hasandel01.meetmeoutserver;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
